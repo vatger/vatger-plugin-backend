a6334961b08ad3da3a9df9b4010bd801508bbf5d
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-02-08 21:51:02 +0100

    service: request model add id

--- src/models/request.py
+++ src/models/request.py
@@ -1,10 +1,13 @@
 import datetime
 from typing import Literal
+from uuid import uuid4
 
 from pydantic import BaseModel, Field
 
 
 class SilentRequestModel(BaseModel):
+    id: str = Field(default_factory=lambda: str(uuid4()))
+
     request_type: Literal["PUSHBACK", "TAXI"]
     callsign: str
     airport_icao: str
