ce0b934c0d00d3a8654fead90b0c0228e1d04030
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-02-08 22:48:56 +0100

    service: request controller implement GET, POST, DELETE

--- src/api/v1/silent_request_controller.py
+++ src/api/v1/silent_request_controller.py
@@ -6,8 +6,21 @@ from fastapi import APIRouter, Depends
 from api.models.request import SilentRequest
 from containers.dependencies import DependencyContainer
 from interfaces.services.request_service_interface import RequestServiceInterface
+from models.request import SilentRequestModel
 
 router = APIRouter(prefix="/request")
+
+
+@router.get("/{callsign}")
+@inject
+def get_request(
+    callsign: str,
+    request_service: Annotated[
+        RequestServiceInterface,
+        Depends(Provide(DependencyContainer.request_container.request_service)),
+    ],
+):
+    return request_service.get_request_by_callsign(callsign)
 
 
 @router.post("")
@@ -19,4 +32,18 @@ def post_request(
         Depends(Provide(DependencyContainer.request_container.request_service)),
     ],
 ):
-    print(f"received {request.callsign}")
+    request = SilentRequestModel(**request.model_dump())
+
+    request_service.create_request(request)
+
+
+@router.delete("/{callsign}")
+@inject
+def delete_request(
+    callsign: str,
+    request_service: Annotated[
+        RequestServiceInterface,
+        Depends(Provide(DependencyContainer.request_container.request_service)),
+    ],
+):
+    request_service.delete_request_by_callsign(callsign)
