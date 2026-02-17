2e67afbe69a5e95027546bda48ca4776ce0584f2
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-02-17 20:57:46 +0100

    service: add mongodb settings

--- src/repositories/mongo/mongo_settings.py
+++ src/repositories/mongo/mongo_settings.py
@@ -1,12 +1,12 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from repositories.mongo.mongo_settings import MongoSettings
 
-
-class Settings(BaseSettings):
-    mongo: MongoSettings = MongoSettings()
+class MongoSettings(BaseSettings):
+    uri: str = "mongodb://localhost:27017"
+    database: str = "vatger-plugin"
 
     model_config = SettingsConfigDict(
+        env_prefix="MONGO_",
         env_file=".env",
         extra="ignore",
     )
