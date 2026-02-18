8d61b370a4b625107c4c48d5b104dc16bc3a5f2b
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-02-18 19:22:29 +0100

    service: add auth adapter

---
+++ src/api/adapters/auth_adapter.py
@@ -0,0 +1,35 @@
+from typing import Annotated
+
+from dependency_injector.wiring import Provide, inject
+from fastapi import Depends, HTTPException, Security
+from fastapi.security import APIKeyHeader
+
+from containers.dependencies import DependencyContainer
+from interfaces.services.auth_service_interface import AuthorizationServiceInterface
+
+API_KEY_NAME = "X-API-Key"
+
+api_key_header = APIKeyHeader(
+    name=API_KEY_NAME,
+)
+
+
+class AuthAdapter:
+    """FastAPI wrapper, checks if the provided API Key has the defined permissions"""
+
+    def __init__(self, *permissions: str):
+        self.required = set(permissions)
+
+    @inject
+    def __call__(
+        self,
+        api_key: str | None = Security(api_key_header),
+        auth_service: Annotated[
+            AuthorizationServiceInterface,
+            Depends(Provide(DependencyContainer.auth_service)),
+        ] = None,
+    ):
+        if not api_key:
+            raise HTTPException(401, "API key missing")
+
+        return auth_service.authorize(api_key, self.required)
