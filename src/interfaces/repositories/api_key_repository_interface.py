633fbc6ccc273b9a9f046c2ca0df4f08e8829f17
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-05-16 15:49:20 +0200

    service: remove api key interfaces

--- src/interfaces/repositories/api_key_repository_interface.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-from domain.api_key import APIKey
-
-
-class APIKeyRepositoryInterface(ABC):
-    @abstractmethod
-    def get_by_hash(self, hash_value: str) -> APIKey: ...
