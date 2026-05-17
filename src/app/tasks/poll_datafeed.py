6a16040fe070a656ed6d88c78762a2f8ebd39986
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-05-17 18:29:21 +0200

    service: tasks - poll_datafeed, add logging

--- src/app/tasks/poll_datafeed.py
+++ src/app/tasks/poll_datafeed.py
@@ -1,3 +1,4 @@
+import logging
 from typing import TYPE_CHECKING
 
 from app.broker import broker
@@ -6,15 +7,21 @@ from containers.dependencies import DependencyContainer
 if TYPE_CHECKING:
     from models.datafeed import DatafeedModel
 
+container = DependencyContainer()
+logger = logging.getLogger(__name__)
+
 
 @broker.task("poll_datafeed")
 async def poll_datafeed():
-    gateway = DependencyContainer().datafeed_container.datafeed_gateway()
-    repository = DependencyContainer().datafeed_container.datafeed_repository()
+    gateway = container.datafeed_container.datafeed_gateway()
+    repository = container.datafeed_container.datafeed_repository()
 
     feed: DatafeedModel = await gateway.fetch()
 
     if feed is None:
+        logger.warning("Feed is None, skipping update")
         return
+
+    logger.info("Fetched feed: %d pilots, %d controllers", len(feed.pilots), len(feed.controllers))
 
     await repository.update(feed)
