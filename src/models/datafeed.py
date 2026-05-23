2da63bd87cfb128433ad5cfccdc1e5fe6fe43e66
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-05-23 20:23:17 +0200

    service: ControllerModel - add isObserver function

--- src/models/datafeed.py
+++ src/models/datafeed.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from enum import IntEnum
 from typing import Literal
 
 from pydantic import BaseModel
@@ -33,18 +34,31 @@ class PilotModel(BaseModel):
     last_updated: datetime
 
 
+class Facility(IntEnum):
+    OBS = 0
+    FSS = 1
+    DEL = 2
+    GND = 3
+    TWR = 4
+    APP = 5
+    CTR = 6
+
+
 class ControllerModel(BaseModel):
     cid: int
     name: str
     callsign: str
     frequency: str
-    facility: int
+    facility: Facility
     rating: int
     server: str
     visual_range: int
     text_atis: list[str] | None = None
     logon_time: datetime
     last_updated: datetime
+
+    def isObserver(self) -> bool:
+        return self.facility == Facility.OBS
 
 
 class DatafeedModel(BaseModel):
