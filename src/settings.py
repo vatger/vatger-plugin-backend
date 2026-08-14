c3fec9b858aa85fb1f4315f7cd265d77540adcd9
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-08-14 18:28:52 +0200

    refactor(settings): VATSIM_CLIENT_SECRET use pydantic SecretStr

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
 
     VATSIM_AUTH_URL: str = "https://auth-dev.vatsim.net"
     VATSIM_CLIENT_ID: str = "1363"
-    VATSIM_CLIENT_SECRET: str = "NFCGxEDK9MobuqHmxJWyTpRWsVGacCH0xPptLU4o"
+    VATSIM_CLIENT_SECRET: SecretStr = SecretStr("NFCGxEDK9MobuqHmxJWyTpRWsVGacCH0xPptLU4o")
     VATSIM_REDIRECT_URL: str = "http://localhost:5173/api/v1/auth/callback"
     VATSIM_AUTH_SCOPES: str = "full_name email vatsim_details country"
     VATSIM_AUTH_USERINFO_PATH: str = "api/user"
